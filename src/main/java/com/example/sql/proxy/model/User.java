5a3ae016c7f4f605c126c93f875c8d9032c83e06
Jeremy Gonzalez <jeremy.gonzalez1128@gmail.com>
2026-05-07 21:47:41 -0400

    add model

---
+++ src/main/java/com/example/sql/proxy/model/User.java
@@ -0,0 +1,17 @@
+package com.example.sql.proxy.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String address;
+}
