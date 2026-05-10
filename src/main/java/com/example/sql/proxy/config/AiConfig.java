5ae7f09929f98a0a1696c78f8c381cfff51848c6
Jeremy Gonzalez <jeremy.gonzalez1128@gmail.com>
2026-05-10 15:28:34 -0400

    give ai instructions onhow to convert message to sql query

--- src/main/java/com/example/sql/proxy/config/AiConfig.java
+++ src/main/java/com/example/sql/proxy/config/AiConfig.java
@@ -9,6 +9,16 @@ public class AiConfig {
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder builder) {
-        return builder.build();
+        return builder.defaultSystem("""
+                you are a bot that takes a message and converts\s
+                that message into an sql script for a postgresql database. the data you return must be ONLY an sql script
+                        and if the message is not related to an sql script return a message saying wrong
+                        message need a message that can be converted to an sql statement.
+                        if theres WHO WHAT in te message those are select statements not inserts or updates
+                        
+                      Current Database Schema:
+                      {schema}
+                        """)
+                .build();
     }
 }
