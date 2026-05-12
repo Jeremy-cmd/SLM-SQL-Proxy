12038f9855284e14595b1929e158353b5e490e46
Jeremy Gonzalez <jeremy.gonzalez1128@gmail.com>
2026-05-12 16:09:45 -0400

    modify chatClient instructions

--- src/main/java/com/example/sql/proxy/config/AiConfig.java
+++ src/main/java/com/example/sql/proxy/config/AiConfig.java
@@ -1,5 +1,7 @@
 package com.example.sql.proxy.config;
 
+import com.example.sql.proxy.service.ProxyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,15 +12,17 @@ public class AiConfig {
     @Bean
     public ChatClient chatClient(ChatClient.Builder builder) {
         return builder.defaultSystem("""
-                you are a bot that takes a message and converts\s
-                that message into an sql script for a postgresql database. the data you return must be ONLY an sql script
-                        and if the message is not related to an sql script return a message saying wrong
-                        message need a message that can be converted to an sql statement.
-                        if theres WHO WHAT in te message those are select statements not inserts or updates
-                        
-                      Current Database Schema:
-                      {schema}
-                        """)
+                        you are a bot that takes a message and converts\s
+                        that message into an sql script for a postgresql database. the data you return must be ONLY an sql script
+                                and if the message is not related to an sql script return a message saying wrong
+                                message need a message that can be converted to an sql statement.
+                                if theres WHO WHAT in te message those are select statements not inserts or updates.
+                                If the user asks for a specific, single person or 'the' user,\s
+                                            include 'LIMIT 1' in the SQL.
+                                        
+                              Current Database Schema:
+                              {schema}
+                                """)
                 .build();
     }
 }
